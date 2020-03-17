de29377d64ea0834521b0096c3b56b64bf31d630
ImRaNM-001 <imu163@gmail.com>
2020-03-18 03:08:16 +0530

    commit all

---
+++ add_2nos.ts
@@ -0,0 +1,9 @@
+const add = (a: number, b: number) => a+b;
+console.log(add(4, 3) );                 // prints 7 where as if i try to remove the data types in next line of codes
+
+// const add = (a, b) => a+b;
+// console.log(add(4, 3) );            // prints nothing
+
+
+const add1 = (a: number, b: string) => a+b;
+console.log(add1(4, '3') );             // prints 43
